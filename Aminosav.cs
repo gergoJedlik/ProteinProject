f6c44f944fe7ab07022c9c4415cfbd9cfbf671dc
Gergo <kapusi.gergo.levente@students.jedlik.eu>
2025-03-03 14:11:52 +0100

    File Reading + JSON deserialization

---
+++ Aminosav.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fehérje
+{
+    internal class Aminosav
+    {
+        public string Nev { get;  set; }
+
+        public string Rovidites { get;  set; }
+
+        public char Betujel { get; set; }
+
+        public int C { get; set; }
+
+        public int H { get; set; }
+
+        public int O { get; set; }
+
+        public int N { get; set; }
+
+        public int S { get; set; }
+
+
+        // public Dictionary<char, int> anyagok { get;  set; }
+        public Aminosav(string data)
+        {
+           
+        }
+    }
+}
---
+++ reader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Fehérje
+{
+    internal class reader
+    {
+        private string json_data { get; }
+
+        public reader(string file)
+        {
+           json_data = File.ReadAllText(file, encoding: UTF8Encoding.UTF8);
+        }
+
+        public List<Aminosav> readJSON()
+        {
+            List<Aminosav> Objects = JsonConvert.DeserializeObject<List<Aminosav>>(json_data);
+
+            return Objects;
+        }
+    }
+}
