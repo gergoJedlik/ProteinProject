71b983d98dc768f708eeac6107d75c3699a0238b
Gergo <kapusi.gergo.levente@students.jedlik.eu>
2025-03-08 21:43:09 +0100

    Writing to File

--- Program.cs
+++ Program.cs
@@ -11,11 +11,25 @@ namespace Fehérje
         static void Main(string[] args)
         {
             Megoldás m = new Megoldás("../../aminosavak.json", "../../bsa.json");
+            Writer writer = new Writer();
+
             //Console.WriteLine(m.SavakSzámaPróba);
             //Console.WriteLine(m.AminosavKiírPróba);
-            Console.WriteLine($"4. Feladat: {m.FehérjeLáncÖsszegképletKiírása}");
-            Console.WriteLine($"6. Feladat: A Factor XI általi hasítás utáni első részletben {m.factorXICiszteinCount} Cisztein található.");
+            //Console.WriteLine($"4. Feladat: {m.FehérjeLáncÖsszegképletKiírása}");
+            //Console.WriteLine($"6. Feladat: A Factor XI általi hasítás utáni első részletben {m.factorXICiszteinCount} Cisztein található.");
+
+            Write(writer, 4, $"{m.FehérjeLáncÖsszegképletKiírása}");
+
+            Write(writer, 6, $"A Factor XI általi hasítás utáni első részletben {m.factorXICiszteinCount} Cisztein található.");
+
+            writer.writeToFile();
             Console.ReadKey();
+        }
+
+        static void Write(Writer w, int taskNum, string ans)
+        {
+            Console.WriteLine($"{taskNum}. Feladat: {ans}");
+            w.addToData(taskNum, ans);
         }
     }
 }
