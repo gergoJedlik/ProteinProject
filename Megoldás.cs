d2fd83c1237d97f649942a073be1a780ad9caf3d
banhidaimatyas <banhidai.matyas@students.jedlik.eu>
2025-03-09 19:16:15 +0100

    comments removed

--- Megoldás.cs
+++ Megoldás.cs
@@ -11,50 +11,6 @@ namespace Fehérje
         private List<Aminosav> aminosavak;
         private List<char> bsaString;
 
-        //public int SavakSzámaPróba
-        //{
-        //    get
-        //    {
-        //        return aminosavak.Count;
-        //    }
-        //}
-        //public Aminosav ElsőAminosavPróba
-        //{
-        //    get
-        //    {
-        //        return aminosavak.First();
-        //    }
-        //}
-        //public string AminosavKiírPróba
-        //{
-        //    get
-        //    {
-        //        string vissza = "";
-        //        vissza += $"\n\tVálasztott aminosav: {ElsőAminosavPróba.Nev}";
-        //        vissza += $"\n\tVálasztott aminosav betűjele: {ElsőAminosavPróba.Betujel}";
-        //        vissza += $"\n\tVálasztott aminosav rövidítése: {ElsőAminosavPróba.Rovidites}";
-        //        vissza += $"\n\tC száma: {ElsőAminosavPróba.C}";
-        //        vissza += $"\n\tH száma: {ElsőAminosavPróba.H}";
-        //        vissza += $"\n\tO száma: {ElsőAminosavPróba.O}";
-        //        vissza += $"\n\tN száma: {ElsőAminosavPróba.N}";
-        //        vissza += $"\n\tS száma: {ElsőAminosavPróba.S}";
-        //        return vissza;
-        //    }
-        //}
-
-
-        //public string BsaKiír
-        //{
-        //    get
-        //    {
-        //        string vissza = "";
-        //        foreach (string s in bsaString)
-        //        {
-        //            vissza += s;
-        //        }
-        //        return vissza;
-        //    }
-        //}
         private List<int> MassOfSubstances
         {
             get
